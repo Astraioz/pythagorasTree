6699694e09ad40a83477c3984797223be126aad4
Maximilian Lucas <max@astraioz.de>
2017-09-24 02:35:44 +0200

    fix

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 <body>
 
 	<div id="description">
+
 		<header>
 			<div style="text-align: center;">
 				<hr />
@@ -26,8 +27,6 @@
 
 		<form id="settingsForm" >
 			<table>
-
-
 				<tr>
 					<td>Iterations</td>
 					<td>
@@ -35,50 +34,52 @@
 						<input type="range" id="iterations" value="10" min="0" max="14" />
 						<!--<input type="checkbox" id="autoIterations" checked="checked"/>
 						<small>auto</small></td>-->
-					</tr>
-					<tr>
-						<td>Base size</td>
-						<td>
-							<!--<input class="textInput" size="4" type="text" id="baseSize" value="150" />-->
-							<input type="range" id="baseSize" value="100" min="0" max="300" />
-						</td>
-					</tr>
-					<tr>
-						<td>α</td>
-						<td>
-							<input type="range" id="angle1" value="50" min="1" max="90" />
-						</td>
-					</tr>
-					<tr>
-						<td>β</td>
-						<td>
-							<input type="range" id="angle2" value="40" min="1" max="90" />
-							<!--<input class="textInput" size="4" type="text" id="angle2" value="40" />-->
-						</td>
-					</tr>
-					<tr>
-						<td>Render triangle lines</td>
-						<td>
-							<input type="checkbox" id="triangleLines"/>
-						</td>
-					</tr>
-					<tr>
-						<td colspan="2">&nbsp;</td>
-					</tr>
-					<tr>
-						<td colspan="2"><input type="button" id="startButton" value="Draw" />
-							&nbsp;
-							<input type="button" id="viewPNG" value="View as PNG" />
-						</td>
-					</tr>
-				</table>
-			</form>
+					</td>
+				</tr>
+				<tr>
+					<td>Base size</td>
+					<td>
+						<!--<input class="textInput" size="4" type="text" id="baseSize" value="150" />-->
+						<input type="range" id="baseSize" value="100" min="0" max="300" />
+					</td>
+				</tr>
+				<tr>
+					<td>α</td>
+					<td>
+						<input type="range" id="angle1" value="50" min="1" max="90" />
+					</td>
+				</tr>
+				<tr>
+					<td>β</td>
+					<td>
+						<input type="range" id="angle2" value="40" min="1" max="90" />
+						<!--<input class="textInput" size="4" type="text" id="angle2" value="40" />-->
+					</td>
+				</tr>
+				<tr>
+					<td>Render triangle lines</td>
+					<td>
+						<input type="checkbox" id="triangleLines"/>
+					</td>
+				</tr>
+				<tr>
+					<td colspan="2">&nbsp;</td>
+				</tr>
+				<tr>
+					<td colspan="2"><input type="button" id="startButton" value="Draw" />
+						&nbsp;
+						<input type="button" id="viewPNG" value="View as PNG" />
+					</td>
+				</tr>
+			</table>
+		</form>
 
-		</div>
+	</div>
 
-		<canvas id="canvas" width="0" height="0"> </canvas>
+	<canvas id="canvas" width="0" height="0"> </canvas>
 
-		<script src="main.js"></script>
-	</body>
+	<script src="main.js"></script>
 
-	</html>
+</body>
+
+</html>
